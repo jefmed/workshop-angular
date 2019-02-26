523401f27624fcd2ef53678edcfb987db027d0e1
Jeferson Medeiros <jeferson.medeiros@gmail.com>
2019-02-26 15:36:20 -0300

    add delivery comp e total da compra

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -10,6 +10,8 @@ import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 8;
+
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
     {label: 'Cartão de Débito', value: 'DEB'},
@@ -20,6 +22,10 @@ export class OrderComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
   }
 
   cartItems(): CartItem[] {
