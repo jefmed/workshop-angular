3b4939838d07b417f132634555d8695aa34852b1
Jeferson Medeiros <jeferson.medeiros@gmail.com>
2019-02-26 16:45:08 -0300

    iplm finalizar pedido

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {RadioOption} from '../shared/radio/radio-option.model';
 import {OrderService} from './order.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
+import {Order, OrderItem} from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -42,6 +43,15 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     return this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id));
+    this.orderService.checkOrder(order)
+      .subscribe((oderId: string) => {
+      console.log(`Compra concluida: ${oderId}`); });
+    this.orderService.clear();
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,15 @@
 import {Injectable} from '@angular/core';
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
+import {Order} from './order.model';
+import {Observable} from 'rxjs/Observable';
+import {Headers, Http, RequestOptions} from '@angular/http';
+import {MEAT_API} from '../app.api';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService) {
+  constructor(private cartService: ShoppingCartService, private http: Http) {
   }
 
   cartItems(): CartItem[] {
@@ -26,5 +30,18 @@ export class OrderService {
 
   itemsValue(): number {
     return this.cartService.total();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append('Content-Tupe', 'application/json')
+    return this.http.post(`${MEAT_API}/orders`,
+      JSON.stringify(order),
+      new RequestOptions({headers: headers}))
+      .map(response => response.json());
+  }
+
+  clear() {
+    this.cartService.clear();
   }
 }
