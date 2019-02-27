2fc5d90f91d3a86c0a37c874255b5edb7269e867
Jeferson Medeiros <jeferson.medeiros@gmail.com>
2019-02-27 14:20:39 -0300

    add order.module

---
+++ src/app/order/order.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {OrderComponent} from './order.component';
+import {DeliveryCostsComponent} from './delivery-costs/delivery-costs.component';
+import {OrderItemsComponent} from './order-items/order-items.component';
+import {SharedModule} from '../shared/shared.module';
+import {RouterModule, Routes} from '@angular/router';
+
+const ROUTES: Routes = [
+  {path: '', component: OrderComponent}
+]
+
+@NgModule({
+  declarations: [OrderComponent, DeliveryCostsComponent, OrderItemsComponent],
+  imports: [SharedModule, RouterModule.forChild(ROUTES)],
+  providers: [],
+})
+export class OrderModule {
+
+}
