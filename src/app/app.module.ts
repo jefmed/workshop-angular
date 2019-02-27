c5c9e11377c558fcb0f8f9663308d097944fe1b6
Jeferson Medeiros <jeferson.medeiros@gmail.com>
2019-02-27 13:38:39 -0300

    refact about.comp, impl module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {ROUTES} from './app.routes';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantComponent} from './restaurants/restaurant/restaurant.component';
 import {RestaurantsService} from './restaurants/restaurants.service';
@@ -24,9 +23,9 @@ import {InputComponent} from './shared/input/input.component';
 import {RadioComponent} from './shared/radio/radio.component';
 import {OrderItemsComponent} from './order/order-items/order-items.component';
 import {OrderService} from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import {DeliveryCostsComponent} from './order/delivery-costs/delivery-costs.component';
+import {OrderSummaryComponent} from './order-summary/order-summary.component';
+import {RatingComponent} from './shared/rating/rating.component';
 
 
 @NgModule({
@@ -34,7 +33,6 @@ import { RatingComponent } from './shared/rating/rating.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
